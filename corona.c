b5683bff94173051b31920458278898afd5bac20
sumedhkaranth <63861635+sumedhkaranth@users.noreply.github.com>
2020-04-19 01:38:48 +0530

    Update corona.c
    Final commit

--- corona.c
+++ corona.c
@@ -72,8 +72,8 @@ int is_equal(int n, int a[n], int y)
 }
 int is_infec(int n, int x,int r, int infec[r1c(n,x)])
 {
-    int flag=0;
-    for(int i=0;i<3;i++)
+    int flag=0, l=r1c(n,x);
+    for(int i=0;i<l;i++)
     {
         if(r==infec[i])
         {
@@ -85,28 +85,26 @@ int is_infec(int n, int x,int r, int infec[r1c(n,x)])
 }            
 int find_first_post(int n, int a[n], int y)
 {
-    int i=0,first=0;
+    int i=0;
     while((a[i]!=y)&&(i<n))
     {
-        first++;
         i++;
     }
-    return first;
+    return i;
 }   
 int find_last_post(int n, int a[n], int y)
 {
-    int i=find_first_post(n,a,y),last=i;
+    int i=find_first_post(n,a,y);
     while((a[i]==y)&&(i<n))
     {
-        last++;
         i++;
     }
-    return last-1;
+    return i-1;
 }
 int give_post(int n, int a[n], int y)
 {
-    int i=0,post=0;
-    while(a[i]!=y)
+    int i=0;
+    while((a[i]!=y)&&(i<n))
     {
         i++;
     }
@@ -119,95 +117,106 @@ void initialize(int n,int infec2[n])
         infec2[i]=0;
     }
 }
-int flagger(int a, int b)
+int reduce(int s, int infec[s], int reduc[s])
 {
-    int flag=0;
-    if(a==b)
+    int size=0, c=0,co=0,l=0,counter=0;
+    for(int i=0;i<s;i++)
     {
-        flag=1;
-    }
-    return flag;
-}   
-int infec_round2(int n, int a[n], int x, int infec2[n], int infec[r1c(n,x)])
-{
-    int flag=0,flag1=0, s=r1c(n,x);
-    int i,size=0,j=0,c=0,ic=0;
-    initialize(n,infec2);
-    printf("\n");
-    for(i=0;i<s;i++)
-    {
-        if(i==0)
+        if(is_equal(s,infec,infec[i])>1)
         {
-            flag=0;
-            flag1=flagger(infec[0],infec[1]);
+            c+=is_equal(s,infec,infec[i]);
+            co++;
         }
-         else if((i!=(s-1))&&(i!=0))
-         {
-            if(flag1==1)
-            {
-                flag=1;
-            }
-            else
-                flag=flagger(infec[i],infec[i+1]);
-         }
-           
         else
-            flag=flagger(infec[i],infec[0]);
-            
-            if(flag==0)
-            {
-                if(((is_equal(n,a,infec[i]))>1))
-                {
-                    int first=find_first_post(n,a,infec[i]);
-                    int last=find_last_post(n,a,infec[i]);
-                    if((is_infec(n,x,a[first-1],infec)==0)&&(first!=0)&&(is_infec(n,x,a[first-1],infec2)==0))
-                    {
-                        infec2[size]=a[first-1];
-                        size++;
-                    }
-                    if((is_infec(n,x,a[last+1],infec)==0)&&(last<(n-1))&&(is_infec(n,x,a[last+1],infec2)==0))
-                    {
-                        infec2[size]=a[last+1];
-                        size++;
-                    }
-                    if(((is_equal(n,infec,infec[i]))!=(is_equal(n,a,infec[i]))))
-                    {
-                        int con=is_equal(n,infec,infec[i]);
-                        for(j=0;j<=(last-first-con+1);j++)
-                        {
-                            
-                            infec2[j+size]=a[first+j];
-                        }
-                        size=size+j-1;
-                    }
-                }
-                else
-                {
-                    int pos=give_post(n,a,infec[i]);
-                    if((pos!=0)&&(is_infec(n,x,a[pos-1],infec)==0)&&(is_infec(n,x,a[pos-1],infec2)==0))
-                    {
-                        infec2[size]=a[pos-1];
-                        size++;
-                    }
-                    if((is_infec(n,x,a[pos+1],infec)==0)&&(pos!=n-1)&&(is_infec(n,x,a[pos+1],infec2)==0))
-                    {
-                        infec2[size]=a[pos+1];
-                        size++;
-                    }
-                    if(i==0)
-                        flag=1;
-                }
-                
-            } 
+            l++;
+    }
+    if(co>0)
+        size=(c/(co*co))+l;
+    else
+        size=l;
+    qsort(infec,s,sizeof(int),compare);
+    int i=0;
+    while(i<s)
+    {
+        if(is_equal(s,infec,infec[i])>1)
+        {
+            reduc[i]=infec[i];
+            i=find_last_post(s,infec,infec[i])+1;
+            counter++;
         }
+        else
+        {
+            if(counter>0)
+            {
+                reduc[i-1]=infec[i];
+                i++;
+            }
+            else if(counter==0)
+            {
+                reduc[i]=infec[i];
+                i++;
+            }   
+        }
+    }
     return size;
 }
-
+int infec_round2(int n, int a[n], int x, int infec2[n], int infec[r1c(n,x)])
+{
+    int flag=0,flag1=0, s=r1c(n,x),check=0;
+    int i,size=0,j=0,si=0;
+    initialize(n,infec2);
+    int reduc[s];
+    si=reduce(s,infec,reduc);
+    for(i=0;i<si;i++)
+    {  
+        if(((is_equal(n,a,reduc[i]))>1))
+        {
+            int first=find_first_post(n,a,reduc[i]);
+            int last=find_last_post(n,a,reduc[i]);
+            if((is_infec(n,x,a[first-1],infec)==0)&&(first!=0)&&(is_infec(n,x,a[first-1],infec2)==0))
+            {
+                infec2[size]=a[first-1];
+                size++;
+            }
+            if((is_infec(n,x,a[last+1],infec)==0)&&(last<(n-1))&&(is_infec(n,x,a[last+1],infec2)==0))
+            {
+                infec2[size]=a[last+1];
+                size++;
+            }
+            check=is_equal(s,infec,infec[i]);
+            for(j=0;j<((is_equal(n,a,infec[i]))-check);j++)
+            {
+                
+                infec2[j+size]=a[first+j];
+            }
+            size=size+j;
+        }
+        else
+        {
+            int pos=give_post(n,a,infec[i]);
+            if((pos!=0)&&(is_infec(n,x,a[pos-1],infec)==0)&&(is_infec(n,x,a[pos-1],infec2)==0))
+            {
+                infec2[size]=a[pos-1];
+                size++;
+            }
+            if((is_infec(n,x,a[pos+1],infec)==0)&&(pos!=n-1)&&(is_infec(n,x,a[pos+1],infec2)==0))
+            {
+                infec2[size]=a[pos+1];
+                size++;
+            }
+        }
+     
+    }
+    return size;
+}
+void display(int a, int b)
+{
+    printf("%d\n%d\n",a,b);
+}
 void find_infec(int n, int a[n], int x, int infec[r1c(n,x)], int infec2[n])
 {
     int os=infec_round2(n,a,x,infec2,infec);
-    infec2[os];
-    int s=r1c(n,x),ti[os+s];
+    int s=r1c(n,x),ti[os+s+1];
     int i=0,j=0;
     while(i<s)
     {
@@ -222,14 +231,13 @@ void find_infec(int n, int a[n], int x, int infec[r1c(n,x)], int infec2[n])
         i++;
         j++;
     }
-    int inf=os+s,death=0;
+    int death=0;
     for(int i=0;i<os+s;i++)
     {
-        if(ti[i]>=60)
+        if(ti[i]>60)
             death++;
     }
-    printf("%d\n",os+s);
-    printf("%d\n",death);
+    display(os+s,death);
 }
 int main()
 {
@@ -238,7 +246,8 @@ int main()
     int a[n];
     input1(n,a);
     key=infected_post();
-    int infec[3], infec2[n];
+    int s=r1c(n,key);               
+    int infec[s], infec2[n];
     infect_round1(n,a,key,infec);
     sort(n,a);
     find_infec(n,a, key, infec,infec2);
